7348f0da04c7f8e9f849ad3daf6ffbd0427e3cd9
Moun <moun.dahiwale@hmhco.com>
2019-11-29 11:51:59 +0000

    Updates

---
+++ precache-manifest.74c94750f9d0160d04c9a214e56ca359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff388d1dcf420c21fdb0c69823b1d03",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "867d0f939242a4874e8a",
+    "url": "/monsters-rolodex/static/css/main.3533a603.chunk.css"
+  },
+  {
+    "revision": "4038d2000d49f4b08045",
+    "url": "/monsters-rolodex/static/js/2.3986c9cf.chunk.js"
+  },
+  {
+    "revision": "867d0f939242a4874e8a",
+    "url": "/monsters-rolodex/static/js/main.88e05648.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);

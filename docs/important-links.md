0a942ef57f483613912a794a1f0f46719fa19537
Moun <moun.dahiwale@hmhco.com>
2019-11-29 11:32:31 +0000

    Added SearchBox component and search functionality

---
+++ docs/important-links.md
@@ -0,0 +1,2 @@
+Introduction to events https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events  
+React SyntheticEvent https://reactjs.org/docs/events.html  
--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list";
 import "./App.css";
+import SearchBox from "./components/search-box/search-box";
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => setMonsters(users));
+      .then(users => {
+        setMonsters(users);
+      });
   }, []);
+
+  useEffect(() => {
+    setFilteredMonsters(
+      monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    );
+  }, [monsters, searchField]);
+
+  const filterMonsters = searchText => {
+    setSearchField(searchText);
+  };
 
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      <SearchBox
+        placeholder="Search Monsters"
+        handleChange={e => filterMonsters(e.target.value)}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
